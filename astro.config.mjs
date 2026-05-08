e91576ac79bf56e2adc30415bdaeee563e930535
Jonathan Hermin <me@familyfriendly.xyz>
2026-05-08 03:58:32 +0200

    stuff and things

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,15 @@ import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+    site: "https://example.com",
+    integrations: [mdx(), sitemap(), icon()],
+    adapter: cloudflare({
+        platformProxy: {
+            enabled: true,
+        },
+    }),
 });
